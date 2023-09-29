32e560af2dc4b1e25f51ace27f69667bd5601683
hi-sanchoman <42446417+hi-sanchoman@users.noreply.github.com>
2023-09-29 21:53:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/cbju', methods=['GET'])
+def calculate_bju():
+    weight = float(request.args.get('bju_weight'))
+    height = float(request.args.get('bju_height'))
+    age = float(request.args.get('bju_age'))
+    gender = request.args.get('bju_gender')
+    percent_fat = float(request.args.get('bju_percent'))
+    activity = float(request.args.get('bju_activity'))
+
+    # Расчет калорий
+    if gender == 'male':
+        calories = (10 * weight) + (6.25 * height) - (5 * age) + 5
+    else:
+        calories = (10 * weight) + (6.25 * height) - (5 * age) - 161
+
+    calories *= (1 + activity)  # учитываем активность
+
+    # Расчет КБЖУ
+    protein = weight * (1.2 - percent_fat)
+    fat = weight * (0.8 - percent_fat)
+    carbs = (calories - (protein * 4 + fat * 9)) / 4
+
+    result = {
+        'calories': round(calories),
+        'protein': round(protein),
+        'fat': round(fat),
+        'carbs': round(carbs)
+    }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
