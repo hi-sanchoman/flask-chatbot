1c18304aa6603e6d5fdf66905d9580aa398ba233
hi-sanchoman <42446417+hi-sanchoman@users.noreply.github.com>
2023-09-29 22:07:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,48 @@ def calculate_bju():
     percent_fat = float(request.args.get('bju_percent')) / 100
     activity = float(request.args.get('bju_activity'))
 
-    # Расчет калорий
-    if gender == 'male':
-        calories = (10 * weight) + (6.25 * height) - (5 * age) + 5
+    # Расчет базального метаболизма
+    if gender == 'Мужской':
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
     else:
-        calories = (10 * weight) + (6.25 * height) - (5 * age) - 161
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
 
-    calories *= (1 + activity)  # учитываем активность
+    # Расчет сухой массы
+    lean_mass = weight * (1 - percent_fat)
+
+    # Расчет термического эффекта пищи (TEF)
+    tef = bmr * 0.1
+
+    # Расчет общего калорийного потребления
+    calories = bmr * (1 + activity) + tef
 
     # Расчет КБЖУ
-    protein = weight * (1.2 - percent_fat)
-    fat = weight * (0.8 - percent_fat)
+    protein = lean_mass * (1.2 - percent_fat)
+    fat = lean_mass * (0.8 - percent_fat)
     carbs = (calories - (protein * 4 + fat * 9)) / 4
 
+    # Расчет ИМТ
+    bmi = weight / ((height / 100) ** 2)
+
+    # Расчет потребности в воде
+    water = weight * 0.035
+
+    # Расчет потребности в клетчатке
+    fiber = 38 if gender == 'Мужской' else 25
+    
     result = {
+        'bmr': round(bmr),
+        'lean_mass': round(lean_mass, 2),
+        'tef': round(tef),
         'calories': round(calories),
         'protein': round(protein),
         'fat': round(fat),
-        'carbs': round(carbs)
+        'carbs': round(carbs),
+        'bmi': round(bmi, 2),
+        'water': round(water, 2),
+        'caffeine': 400,  # максимальное рекомендуемое потребление кофеина
+        'fiber': fiber,
+        'salt': 5  # рекомендуемое потребление соли
     }
 
     return jsonify(result)
